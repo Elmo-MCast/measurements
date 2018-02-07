7ca3947a06fd6f450646ba183dbde819ff4d54fa
Muhammad Shahbaz <mshahbaz@cs.princeton.edu>
2018-02-07 14:03:08 -0500

    minor

--- scripts/install_packages.r
+++ scripts/install_packages.r
@@ -1,1 +1,1 @@
-install.packages(c("Hmisc", "ggplot2", "data.table", "tibble", "plotly"), repos="http://cran.us.r-project.org")
+install.packages(c("Hmisc", "ggplot2", "data.table", "tibble", "plotly", "bit64"), repos="http://cran.us.r-project.org")
